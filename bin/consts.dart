bee9c4b5235c96da1e59c3d672ad93b467e0b90f
carloshenrique-dev <c.tarabal25@gmail.com>
2023-05-23 12:26:24 -0300

    first commit

---
+++ bin/consts.dart
@@ -0,0 +1,1 @@
+const String key = "f39c88061b2e6c8a8f23ec2994425f9f";
---
+++ bin/server.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart';
+import 'package:shelf_router/shelf_router.dart';
+import 'package:http/http.dart' as http;
+
+// Configure routes.
+final _router = Router()
+  ..get('/', _rootHandler)
+  ..get('/search/<query>', _echoHandler);
+
+Response _rootHandler(Request req) {
+  return Response.ok('Google Search API!\n');
+}
+
+Future<Response> _echoHandler(Request request) async {
+  final query = request.params['query'];
+  if (query != null) {
+    final result = await searchOnGoogle(query);
+    return Response.ok(result);
+  }
+  return Response.badRequest(body: 'Missing query param');
+}
+
+Future<String> searchOnGoogle(String query) async {
+  final encodedQuery = Uri.encodeComponent(query);
+  final url =
+      'https://www.googleapis.com/customsearch/v1?key=AIzaSyAZ-46yaXErqJRxr8uxeyepg0knGuD0f6o&cx=24ff674370c5c406b&q=$encodedQuery';
+  final response = await http.get(Uri.parse(url));
+  return response.body;
+}
+
+void main(List<String> args) async {
+  final ip = InternetAddress.anyIPv4;
+
+  final handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
+
+  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final server = await serve(handler, ip, port);
+  print('Server listening on port ${server.port}');
+}
